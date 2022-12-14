f2dd7017f9fd3f7ad8e700b4cab362b8a995f678
unknown <sk.mdismail056@gmail.com>
2022-12-14 19:54:00 +0530

    signin,signup

---
+++ Header,Footer/index.js
@@ -0,0 +1,8 @@
+// let user=document.getElementById("UserName")
+// user.addEventListener("click",()=>{
+    
+// })
+// let home=document.getElementById("logon").addEventListener("click",homepage)
+// function homepage(){
+//     window.location.href="index.html"
+// }
